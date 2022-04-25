3d0121c26959f92ea17f3b90d78de4bba6cc0b4f
David Lee <david.x.lee@ny.email.gs.com>
2022-04-25 15:35:25 -0400

    Export persistence models, protocols, and transforms for use by dependent modules.

--- packages/legend-extension-dsl-persistence/src/index.ts
+++ packages/legend-extension-dsl-persistence/src/index.ts
@@ -16,3 +16,37 @@
 
 export * from './DSLPersistence_Extension';
 export { DSLPersistence_LegendStudioPlugin } from './components/studio/DSLPersistence_LegendStudioPlugin';
+
+// ---------- PACKAGEABLE ELEMENT ----------
+
+// metamodels
+export * from './models/metamodels/pure/model/packageableElements/persistence/DSLPersistence_Auditing';
+export * from './models/metamodels/pure/model/packageableElements/persistence/DSLPersistence_DeduplicationStrategy';
+export * from './models/metamodels/pure/model/packageableElements/persistence/DSLPersistence_IngestMode';
+export * from './models/metamodels/pure/model/packageableElements/persistence/DSLPersistence_MergeStrategy';
+export * from './models/metamodels/pure/model/packageableElements/persistence/DSLPersistence_Milestoning';
+export * from './models/metamodels/pure/model/packageableElements/persistence/DSLPersistence_Notifier';
+export * from './models/metamodels/pure/model/packageableElements/persistence/DSLPersistence_Persistence';
+export * from './models/metamodels/pure/model/packageableElements/persistence/DSLPersistence_Persister';
+export * from './models/metamodels/pure/model/packageableElements/persistence/DSLPersistence_Sink';
+export * from './models/metamodels/pure/model/packageableElements/persistence/DSLPersistence_TargetShape';
+export * from './models/metamodels/pure/model/packageableElements/persistence/DSLPersistence_Trigger';
+
+// v1 protocols
+export * from './models/protocols/pure/v1/model/packageableElements/persistence/V1_DSLPersistence_Auditing';
+export * from './models/protocols/pure/v1/model/packageableElements/persistence/V1_DSLPersistence_DeduplicationStrategy';
+export * from './models/protocols/pure/v1/model/packageableElements/persistence/V1_DSLPersistence_IngestMode';
+export * from './models/protocols/pure/v1/model/packageableElements/persistence/V1_DSLPersistence_MergeStrategy';
+export * from './models/protocols/pure/v1/model/packageableElements/persistence/V1_DSLPersistence_Milestoning';
+export * from './models/protocols/pure/v1/model/packageableElements/persistence/V1_DSLPersistence_Notifier';
+export * from './models/protocols/pure/v1/model/packageableElements/persistence/V1_DSLPersistence_Persistence';
+export * from './models/protocols/pure/v1/model/packageableElements/persistence/V1_DSLPersistence_Persister';
+export * from './models/protocols/pure/v1/model/packageableElements/persistence/V1_DSLPersistence_Sink';
+export * from './models/protocols/pure/v1/model/packageableElements/persistence/V1_DSLPersistence_TargetShape';
+export * from './models/protocols/pure/v1/model/packageableElements/persistence/V1_DSLPersistence_Trigger';
+
+// ---------- TRANSFORMATION ----------
+
+export * from './models/protocols/pure/v1/transformation/pureGraph/from/V1_PersistenceTransformer';
+export * from './models/protocols/pure/v1/transformation/pureGraph/to/V1_PersistenceBuilder';
+export * from './models/protocols/pure/v1/transformation/pureProtocol/V1_DSLPersistence_ProtocolHelper';
